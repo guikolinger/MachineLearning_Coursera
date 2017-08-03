c2ff3ab0a8d26b4a2102efd2f3ee41dff4d6cf7e
g.domingues.kolinger <guikolinger@gmail.com>
2017-08-03 15:46:20 +0200

    update of assignment
    first two parts are done

--- Assignment 6 - Spam and Support Vector Machines/dataset3Params.m
+++ Assignment 6 - Spam and Support Vector Machines/dataset3Params.m
@@ -23,12 +23,33 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+errorBest = realmax();
+Cbest = C;
+sigmaBest = sigma;
 
+tests = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
+for ii = 1:size(tests,2)
+  for jj = 1:size(tests,2)
+    C = tests(ii);
+    sigma = tests(jj);
+    
+    model = svmTrain(X, y, C, @(x1,x2) gaussianKernel(x1,x2,sigma));
+    predictions = svmPredict(model, Xval);
+    error = mean(double(predictions ~= yval));
+    
+    if error < errorBest
+      errorBest = error;
+      Cbest = C;
+      sigmaBest = sigma;
+    end
+  end
+end
 
-
-
-
+C = Cbest;
+sigma = sigmaBest;
+      
+  
 % =========================================================================
 
 end
